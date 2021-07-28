9dc9439046690253726acf8e79f1997e9f6a86f4
Loïc Vincent-Genod <loic.vincent-genod@consensys.net>
2021-07-28 17:32:13 +0200

    feat: add integration tests

--- testing/integration/main_test.go
+++ testing/integration/main_test.go
@@ -3,10 +3,195 @@
 package integration
 
 import (
+	"context"
+	"crypto/ecdsa"
+	"log"
+	"math/big"
 	"os"
 	"testing"
+	"time"
+
+	"github.com/VGLoic/godel/ethshell"
+	"github.com/VGLoic/godel/ethshell/contract"
+	"github.com/VGLoic/godel/eventlog"
+	"github.com/VGLoic/godel/ipfsshell"
+	"github.com/VGLoic/godel/node"
+	"github.com/VGLoic/godel/testing/dockerclient"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/strslice"
+	"github.com/docker/go-connections/nat"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/spf13/viper"
 )
 
 func TestMain(m *testing.M) {
-	os.Exit(m.Run())
+	ctx := context.Background()
+
+	viper.SetEnvPrefix("INTEGRATION")
+	viper.AutomaticEnv()
+
+	cli, err := dockerclient.NewDockerCli()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mnemonic := viper.GetString("MNEMONIC")
+
+	_, err = createPostgresContainer(ctx, cli)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = createGanacheContainer(ctx, cli, mnemonic)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	privateKeyHex := viper.GetString("PRIVATE_KEY")
+	address, contractAddress, err := setupContract(ctx, privateKeyHex)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	eventLogConfig := eventlog.EventLogConfiguration{
+		PostgresHost:     "localhost",
+		PostgresUser:     "admin",
+		PostgresPassword: "admin",
+		PostgresDb:       "event",
+		PostgresPort:     "5432",
+	}
+	ipfsShellConfig := ipfsshell.ShellConfiguration{
+		IpfsNodeUrl:   viper.GetString("IPFS_DAEMON_URL"),
+		ProjectId:     viper.GetString("IPFS_PROJECT_ID"),
+		ProjectSecret: viper.GetString("IPFS_PROJECT_SECRET"),
+	}
+	ethShellConfig := ethshell.ShellConfiguration{
+		EthNodeUrl:      "ws://localhost:7545",
+		ContractAddress: contractAddress,
+		PrivateKey:      privateKeyHex,
+	}
+	godelNode, err := node.NewGodelNode(
+		ctx,
+		eventLogConfig,
+		ipfsShellConfig,
+		ethShellConfig,
+		address,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = godelNode.Start(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go func() {
+		log.Fatal(godelNode.ServeApi())
+	}()
+
+	exitCode := m.Run()
+
+	cli.StopContainers(ctx)
+
+	os.Exit(exitCode)
+}
+
+func createPostgresContainer(ctx context.Context, cli *dockerclient.DockerCli) (container.ContainerCreateCreatedBody, error) {
+	hostBinding := nat.PortBinding{
+		HostIP:   "0.0.0.0",
+		HostPort: "5432",
+	}
+
+	containerPort, err := nat.NewPort("tcp", "5432")
+	if err != nil {
+		return container.ContainerCreateCreatedBody{}, err
+	}
+	portBinding := nat.PortMap{containerPort: []nat.PortBinding{hostBinding}}
+	hostConfig := container.HostConfig{
+		PortBindings: portBinding,
+	}
+	env := []string{
+		"POSTGRES_PORT=5432",
+		"POSTGRES_DB=event",
+		"POSTGRES_PASSWORD=admin",
+		"POSTGRES_USER=admin",
+	}
+	containerConfig := container.Config{
+		Image: "postgres",
+		Env:   env,
+	}
+	return cli.CreateContainer(ctx, &containerConfig, &hostConfig, "db")
+}
+func createGanacheContainer(ctx context.Context, cli *dockerclient.DockerCli, mnemonic string) (container.ContainerCreateCreatedBody, error) {
+	hostBinding := nat.PortBinding{
+		HostIP:   "0.0.0.0",
+		HostPort: "7545",
+	}
+
+	containerPort, err := nat.NewPort("tcp", "8545")
+	if err != nil {
+		return container.ContainerCreateCreatedBody{}, err
+	}
+	portBinding := nat.PortMap{containerPort: []nat.PortBinding{hostBinding}}
+	hostConfig := container.HostConfig{
+		PortBindings: portBinding,
+	}
+	containerConfig := container.Config{
+		Image: "trufflesuite/ganache-cli",
+		Cmd:   strslice.StrSlice{"-m " + mnemonic},
+	}
+	return cli.CreateContainer(ctx, &containerConfig, &hostConfig, "ganache")
+}
+
+func setupContract(ctx context.Context, privateKeyHex string) (string, string, error) {
+	retryCount := 0
+	isConnected := false
+	var client *ethclient.Client
+	var err error
+	for retryCount < 10 && !isConnected {
+		client, err = ethclient.Dial("http://localhost:7545")
+		if err == nil {
+			_, err = client.BlockNumber(ctx)
+			if err == nil {
+				isConnected = true
+				break
+			}
+		}
+
+		retryCount += 1
+		time.Sleep(1 * time.Second)
+	}
+	if err != nil {
+		return "", "", err
+	}
+
+	privateKey, err := crypto.HexToECDSA(privateKeyHex)
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, _ := publicKey.(*ecdsa.PublicKey)
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+	nonce, err := client.PendingNonceAt(ctx, fromAddress)
+	if err != nil {
+		return "", "", err
+	}
+
+	gasPrice, err := client.SuggestGasPrice(ctx)
+	if err != nil {
+		return "", "", err
+	}
+
+	auth := bind.NewKeyedTransactor(privateKey)
+	auth.Nonce = big.NewInt(int64(nonce))
+	auth.Value = big.NewInt(0)      // in wei
+	auth.GasLimit = uint64(3000000) // in units
+	auth.GasPrice = gasPrice
+	auth.Context = ctx
+
+	address, _, _, err := contract.DeployContract(auth, client)
+	if err != nil {
+		return "", "", err
+	}
+	return fromAddress.String(), address.String(), nil
 }
--- testing/integration/publishevent_test.go
+++ testing/integration/publishevent_test.go
@@ -3,35 +3,101 @@
 package integration
 
 import (
-	"fmt"
 	"net/rpc"
+	"regexp"
 	"testing"
+	"time"
 
 	"github.com/VGLoic/godel/api"
 	"github.com/VGLoic/godel/eventlog"
+	_require "github.com/stretchr/testify/require"
 )
 
 func TestPublishEvent(t *testing.T) {
+	require := _require.New(t)
+
 	serverAddress := "localhost:1234"
-	rpcClient, dialErr := rpc.DialHTTP("tcp", serverAddress)
-	if dialErr != nil {
-		t.Errorf("err = %v; want %v", dialErr, nil)
-	}
+	rpcClient, err := rpc.DialHTTP("tcp", serverAddress)
+	require.Nil(err)
 	defer rpcClient.Close()
 
-	args := api.PublishEventArgs{
+	publishEventArgs := api.PublishEventArgs{
 		Type:        "integration/type",
 		Payload:     "{}",
 		Version:     0.1,
 		Topic:       "integration",
-		NewAccounts: []string{"hello"},
+		NewAccounts: []string{},
 	}
-	reply := eventlog.Event{}
-	callErr := rpcClient.Call("Api.PublishEvent", &args, &reply)
-	if callErr != nil {
-		t.Errorf("err = %v; want %v", callErr, nil)
+	publishedEvent := publishEvent(rpcClient, require, publishEventArgs)
+
+	waitForConfirmation(rpcClient, require, publishEventArgs, publishedEvent)
+}
+
+func publishEvent(rpcClient *rpc.Client, require *_require.Assertions, publishEventArgs api.PublishEventArgs) eventlog.Event {
+	publishEventReply := eventlog.Event{}
+	err := rpcClient.Call("Api.PublishEvent", &publishEventArgs, &publishEventReply)
+	require.Nil(err)
+
+	checkEvent(require, publishEventArgs, publishEventReply)
+	require.Regexp(regexp.MustCompile("^0x[0-9a-fA-F]{64}$"), publishEventReply.TxHash, "TxHash should have proper format")
+
+	return publishEventReply
+}
+
+func waitForConfirmation(rpcClient *rpc.Client, require *_require.Assertions, publishEventArgs api.PublishEventArgs, publishedEvent eventlog.Event) eventlog.Event {
+	retryCount := 0
+	isConfirmed := false
+	var confirmedEvent eventlog.Event
+	for retryCount < 10 && !isConfirmed {
+		var queryArgs interface{}
+		var confirmedQueryReply []eventlog.Event
+		err := rpcClient.Call("Api.FindConfirmedEvents", &queryArgs, &confirmedQueryReply)
+		require.Nil(err)
+		isContained, event := contains(confirmedQueryReply, publishedEvent.ID)
+
+		if isContained {
+			confirmedEvent = event
+			isConfirmed = true
+		} else {
+			var pendingQueryReply []eventlog.Event
+			err = rpcClient.Call("Api.FindPendingEvents", &queryArgs, &pendingQueryReply)
+			require.Nil(err)
+			isContained, event := contains(pendingQueryReply, publishedEvent.ID)
+			require.Equal(isContained, true, "Event must be pending")
+			checkEvent(require, publishEventArgs, event)
+			require.Equal(event.Timestamp, uint64(0), "Timestamp should be equal to 0")
+			require.Equal(event.BlockNumber, uint64(0), "Blocknumber should be equal to 0")
+			require.Equal(event.Depth, uint64(0), "Depth should be equal to 0")
+
+			retryCount += 1
+			time.Sleep(1 * time.Second)
+		}
+
 	}
-	fmt.Println("REPLY: ", reply)
-	// reply:
-	// {{0 0001-01-01 00:00:00 +0000 UTC 0001-01-01 00:00:00 +0000 UTC {0001-01-01 00:00:00 +0000 UTC false}} 8 integration/type {} 0.1 integration bafyreiags3je4wzlkagusfwh4yufoyymware2lnaq4n7h74eonjcjqmrcq 0xf61B443A155b07D2b2cAeA2d99715dC84E839EEf [hello] 0 0 0 0xcbd4f8f02c48a031ad8e8a35433fcfdf8044563800cea7bc5c514aec87a9e855 2021-07-19 15:17:11.291413076 +0000 +0000 2021-07-19 15:17:11.291413076 +0000 +0000}
+
+	checkEvent(require, publishEventArgs, confirmedEvent)
+	require.Greater(confirmedEvent.Timestamp, uint64(0), "Timestamp should be greater than 0")
+	require.Greater(confirmedEvent.BlockNumber, uint64(0), "Blocknumber should be greater than 0")
+	require.Greater(confirmedEvent.Depth, uint64(0), "Depth should be greater than 0")
+
+	return confirmedEvent
+}
+
+func checkEvent(require *_require.Assertions, publishEventArgs api.PublishEventArgs, event eventlog.Event) {
+	require.Equal(event.Type, publishEventArgs.Type, "Type should match")
+	require.Equal(event.Payload, publishEventArgs.Payload, "Payload should match")
+	require.Equal(event.Version, publishEventArgs.Version, "Version should match")
+	require.Equal(event.Topic, publishEventArgs.Topic, "Topic should match")
+	require.ElementsMatch(event.NewAccounts, publishEventArgs.NewAccounts, "New accounts should match")
+	require.Equal(event.Cid, "bafyreiags3je4wzlkagusfwh4yufoyymware2lnaq4n7h74eonjcjqmrcq", "Cid should be the expected one")
+	require.Greater(event.ID, uint(0), "ID should be greater or equal to one")
+}
+
+func contains(arr []eventlog.Event, id uint) (bool, eventlog.Event) {
+	for _, e := range arr {
+		if e.ID == id {
+			return true, e
+		}
+	}
+	return false, eventlog.Event{}
 }

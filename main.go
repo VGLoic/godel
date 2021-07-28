f842b08587d0594a824aa2656800ffd4d399fb5d
Loïc Vincent-Genod <loic.vincent-genod@consensys.net>
2021-07-28 17:31:22 +0200

    feat: move configuration definition at main level

--- main.go
+++ main.go
@@ -5,23 +5,56 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/VGLoic/godel/config"
+	"github.com/VGLoic/godel/ethshell"
+	"github.com/VGLoic/godel/eventlog"
+	"github.com/VGLoic/godel/ipfsshell"
 	"github.com/VGLoic/godel/node"
+	"github.com/spf13/viper"
 )
 
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
+	err := config.LoadConfig()
+	if err != nil {
+		panic(fmt.Errorf("Fatal error configuration: %s \n", err))
+	}
+
 	fmt.Println("-- Creating Godel Node --")
-	godelNode, godelNodeErr := node.NewGodelNode(ctx)
-	if godelNodeErr != nil {
-		panic(fmt.Errorf("Fatal error godel node: %s \n", godelNodeErr))
+	eventLogConfig := eventlog.EventLogConfiguration{
+		PostgresHost:     viper.GetString("POSTGRES_HOST"),
+		PostgresUser:     viper.GetString("POSTGRES_USER"),
+		PostgresPassword: viper.GetString("POSTGRES_PASSWORD"),
+		PostgresDb:       viper.GetString("POSTGRES_DB"),
+		PostgresPort:     viper.GetString("POSTGRES_PORT"),
+	}
+	ipfsShellConfig := ipfsshell.ShellConfiguration{
+		IpfsNodeUrl:   viper.GetString("IPFS_DAEMON_URL"),
+		ProjectId:     viper.GetString("IPFS_PROJECT_ID"),
+		ProjectSecret: viper.GetString("IPFS_PROJECT_SECRET"),
+	}
+	ethShellConfig := ethshell.ShellConfiguration{
+		EthNodeUrl:      viper.GetString("ETH_NODE_URL"),
+		ContractAddress: viper.GetString("CONTRACT_ADDRESS"),
+		PrivateKey:      viper.GetString("PRIVATE_KEY"),
+	}
+	godelNode, err := node.NewGodelNode(
+		ctx,
+		eventLogConfig,
+		ipfsShellConfig,
+		ethShellConfig,
+		viper.GetString("ACCOUNT_ADDRESS"),
+	)
+	if err != nil {
+		panic(fmt.Errorf("Fatal error godel node: %s \n", err))
 	}
 
 	fmt.Println("-- Starting Godel Node --")
-	startErr := godelNode.Start(ctx)
-	if startErr != nil {
-		panic(fmt.Errorf("Fatal error godel node start: %s \n", startErr))
+	err = godelNode.Start(ctx)
+	if err != nil {
+		panic(fmt.Errorf("Fatal error godel node start: %s \n", err))
 	}
 
 	fmt.Println("-- Serving the API --")
